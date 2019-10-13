7640412bb721c4488fccf014f4dbe66d8ff0f933
Pranav Hegde <pranavh4@gmail.com>
2019-10-13 22:26:28 +0530

    Added Lab 4

--- Lab 4/Validate_Test.java
+++ Lab 4/Validate_Test.java
@@ -1,5 +1,14 @@
 import java.util.*;
 import VehicleNumberPlate.Validate;
 public class Validate_Test {
-	Scanner sc = new Sc
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter license plate number: ");
+		String plate = sc.nextLine();
+		if(Validate.check_number_plate(plate))
+			System.out.println("Valid Number plate");
+		else
+			System.out.println("Invalid Number Plate");
+	}
 }
